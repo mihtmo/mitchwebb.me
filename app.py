ff8d4ff5ef45b111557948750c1fdeb926f54a0a
wilchett <mitch.webb@att.net>
2021-12-28 23:48:23 -0600

    Everything's linking together!

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def frontpage():
+    # Show front page
+    return render_template("frontpage.html")
+
+@app.route("/spottedlanternfly")
+def spottedlanterfly():
+    # Show lanterfly page
+    return render_template("spottedlanternfly.html")
