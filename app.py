babc255f094cf5be7b802792f4de8f0174dcc9c2
wilchett <mitch.webb@att.net>
2023-02-24 00:56:32 -0600

    update to main icon (marimba now)

--- app.py
+++ app.py
@@ -16,62 +16,6 @@ def frontpage():
 
 @app.route("/spottedlanternfly")
 def slfly():
-    ### Original Database Method Abandoned after Heroku Changed Pricing ###
-    # # Establish connection with Heroku PSQL DB
-    # DATABASE_URL = os.environ.get('HEROKU_POSTGRESQL_COPPER_URL')
-    # conn = psycopg2.connect(DATABASE_URL)
-    
-    # try:
-    #     # Create cursor
-    #     cur = conn.cursor()
-    #     # See if there are any rows in databas
-    #     cur.execute("SELECT EXISTS (SELECT 1 FROM observations)")
-    #     ispopulated = cur.fetchall()
-        
-    #     # If database is empty, just load page
-    #     if ispopulated == False:
-    #         print("No new rows in database")
-        
-    #     # If database contains rows, add any missing data points to file on Heroku
-    #     else:
-    #         with open('static/js/observations.js', 'r+') as f:
-    #             # This could be done more efficiently (if it made much of a difference) by only reading
-    #             # the number of instances of the string "Feature", multiplied by the value of rowcount from
-    #             # the END of the file, and loading that chunk into readfile.
-    #             cur.execute("SELECT * FROM observations")
-    #             observationstable = cur.fetchall()
-    #             readfile = f.read()
-    #             # Prepare file for new data
-    #             f.seek(0, 2)
-    #             f.seek(f.tell() - 2, os.SEEK_SET)
-    #             f.truncate()
-    #             for row in observationstable:
-    #                 currentID = row[0]
-    #                 # Check if new data has already been added
-    #                 print(currentID)
-    #                 if str(currentID) in readfile: 
-    #                     print("Observations already contained in dataset")
-    #                     continue
-    #                 else: 
-    #                     print("New data:")
-    #                     f.write(",")
-    #                     f.write(f"{{\"type\":\"Feature\",\"properties\":{{\"id\":{row[0]},\"time\":"
-    #                             f"\"{row[1]}\",\"latitude\":{row[2]},\"longitude\":"
-    #                             f"{row[3]},\"place\":\"{row[4]}\",\"inaturl\":\"{row[5]}\","
-    #                             f"}}, \"geometry\":{{\"type\":\"Point\", "
-    #                             f"\"coordinates\":[{row[3]},{row[2]}]}}}}")
-    #             # Close file syntax
-    #             f.write("]}")
-    #             f.close()
-
-    # except (Exception, psycopg2.DatabaseError) as error:
-    #     print(error)
-
-    # finally:
-    #     if conn is not None:
-    #         conn.close()
-    #         print('Database connection closed')
-    # Open QGIS map
     return render_template("lanternfly.html")
 
 
